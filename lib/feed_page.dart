0145970826fcb2f9d01aba71f1c867870aad2276
CraigFoote <CraigFoote@gmail.com>
2022-01-15 23:00:57 -0500

    encapsulation

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -2,13 +2,12 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dart_rss/dart_rss.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:http/http.dart' as http;
 import 'package:rss_feeds/rss_exception.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'channel_set.dart';
 import 'feed_item.dart';
+import 'feed_item_page.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({required this.channels, required this.client, Key? key})
@@ -175,124 +174,5 @@ class _FeedPageState extends State<FeedPage> {
     Navigator.of(context).push(MaterialPageRoute(
         builder: (_) =>
             FeedItemPage(client: widget.client, feedItem: feedItem)));
-  }
-}
-
-class FeedItemPage extends StatefulWidget {
-  const FeedItemPage({required this.client, required this.feedItem, Key? key})
-      : super(key: key);
-
-  final FeedItem feedItem;
-  final http.Client client;
-
-  @override
-  State<StatefulWidget> createState() => _FeedItemPageState();
-}
-
-class _FeedItemPageState extends State<FeedItemPage> {
-  @override
-  Widget build(BuildContext context) {
-    String? url;
-    if (widget.feedItem.rssFeed.image != null &&
-        widget.feedItem.rssFeed.image?.url != null) {
-      url = widget.feedItem.rssFeed.image?.url;
-    } else {
-      url =
-          'https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Feed-icon.svg/128px-Feed-icon.svg.png';
-    }
-    return FutureBuilder(
-      future: getContent(widget.feedItem.rssItem.link),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(color: Color(0xff4c566a)),
-          );
-        }
-        String data = snapshot.data as String;
-        return Material(
-          child: Scaffold(
-            appBar: AppBar(title: const Text('RSS Feed Item')),
-            body: Column(
-              children: [
-                ListTile(
-                  isThreeLine: true,
-                  leading: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: Hero(
-                      tag: widget.feedItem.rssItem.title!,
-                      child: CachedNetworkImage(
-                        height: 50,
-                        imageUrl: url!,
-                      ),
-                    ),
-                  ),
-                  title: SelectableText(
-                    widget.feedItem.rssItem.title!,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  subtitle: Html(
-                      onLinkTap: (url, context, attributes, element) =>
-                          _launchInBrowser(url!),
-                      style: {
-                        'body': Style(
-                          fontSize: FontSize.large,
-                          backgroundColor: const Color(0xffd8dee9),
-                          padding: const EdgeInsets.all(8),
-                        ),
-                      },
-                      data: widget.feedItem.rssFeed.title! +
-                          '\n\n' +
-                          widget.feedItem.rssItem.description! +
-                          '\n\n' +
-                          widget.feedItem.rssItem.pubDate!),
-                  tileColor: const Color(0xff81a1c1),
-                  dense: false,
-                  contentPadding: const EdgeInsets.all(8),
-                  textColor: const Color(0xff4c566a),
-                  hoverColor: const Color(0xffebcb8b),
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Html(
-                      onLinkTap: (url, context, attributes, element) =>
-                          _launchInBrowser(url!),
-                      style: {
-                        'body': Style(
-                          fontSize: FontSize.xLarge,
-                          backgroundColor: const Color(0xffd8dee9),
-                          padding: const EdgeInsets.all(8),
-                        ),
-                      },
-                      data: data,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Future<void> _launchInBrowser(String url) async {
-    if (!await launch(
-      url,
-      forceWebView: false,
-    )) {
-      throw 'Could not launch $url';
-    }
-  }
-
-  Future<String> getContent(String? link) async {
-    String body = '';
-    await widget.client.get(Uri.parse(link!)).then((response) {
-      body = response.body;
-    });
-    return body;
   }
 }
